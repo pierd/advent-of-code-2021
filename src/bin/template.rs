aac4349834cd061171d9369454b149d8676fcafe
Jakub Jaroszewski <jakub.jaroszewski@gmail.com>
2021-12-20 20:03:55 +1100

    Add template

---
+++ src/bin/template.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Part 1: {}", 0);
+    println!("Part 2: {}", 0);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn dummy() {
+        assert_eq!(2 + 2, 4);
+    }
+}
